5f9581e4d6ce94718691c789d3ecc338a4a597c7
Eugen Barbu <liviu.barbu@ro.ibm.com>
2022-06-30 19:39:48 +0300

    Curs 4

--- src/main/java/com/demo/springdemo/controller/IndexController.java
+++ src/main/java/com/demo/springdemo/controller/IndexController.java
@@ -4,6 +4,8 @@ import com.demo.springdemo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -12,6 +14,16 @@ public class IndexController {
 
     @GetMapping(value = "/index")
     public String index(Model model){
+        Employee myEmployee = Employee.builder()
+                .age(24)
+                .location("Galati")
+                .name("Max")
+                .active(true)
+                .salary(1600)
+                .department("Logists")
+                .build();
+
+        System.out.println(myEmployee.toString());
 
         String text = "Hello from Spring Controller";
 
@@ -22,12 +34,31 @@ public class IndexController {
         model.addAttribute("fruits", fruits);
 
         List<Employee> employeeList = List.of(
-                new Employee("Max", 6500, "IT Support"),
+               /* new Employee("Max", 6500, "IT Support"),
                 new Employee("George", 8650, "Management"),
-                new Employee("Andreea", 7345, "Logistics")
+                new Employee("Andreea", 7345, "Logistics")*/
         );
         model.addAttribute("employeeList", employeeList);
 
         return "index";
+    }
+
+    @GetMapping(value = "/employeeForm")
+    public String employeeForm(Model model){
+        Employee myEmployee = Employee.builder().build();
+        myEmployee.setName("Heloo");
+        model.addAttribute("employee", myEmployee);
+        return "employeeForm";
+    }
+
+    @PostMapping(value = "/submitEmployee")
+    public String submitEmployee(@ModelAttribute Employee employee){
+        System.out.println(employee.toString());
+        saveToDatbase(employee);
+        return "index";
+    }
+
+    private void saveToDatbase(Employee employee) {
+        //TODO save to database
     }
 }
--- src/main/java/com/demo/springdemo/model/Employee.java
+++ src/main/java/com/demo/springdemo/model/Employee.java
@@ -1,38 +1,18 @@
 package com.demo.springdemo.model;
 
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@Builder
 public class Employee {
 
     private String name;
     private int salary;
     private String department;
+    private boolean active;
+    private int age;
+    private String location;
 
-    public Employee(String name, int salary, String department) {
-        this.name = name;
-        this.salary = salary;
-        this.department = department;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
 }
