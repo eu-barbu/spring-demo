6ae0545cd650644ebd68f880a7ffb734f923a020
Eugen Barbu <liviu.barbu@ro.ibm.com>
2022-07-11 10:11:36 +0300

    Curs 7

--- src/main/java/com/demo/springdemo/controller/EmployeeController.java
+++ src/main/java/com/demo/springdemo/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -43,6 +44,23 @@ public class EmployeeController {
     @PostMapping(value = "/submitEmployee")
     public String submitEmployee(@ModelAttribute Employee employee, Model model){
         employeeRepository.save(employee);
+        return "redirect:/employeeOverview";
+    }
+
+    @PostMapping(value = "/editEmployee")
+    public String editEmployee(@RequestParam("employeeId") int id, Model model){
+        Employee employee = employeeRepository.findById(id).get();
+        model.addAttribute("employee", employee);
+
+        List<Department> departmentList = departmentRepository.findAll();
+        model.addAttribute("departmentList", departmentList);
+
+        return "employeeForm";
+    }
+
+    @PostMapping(value = "/deleteEmployee")
+    public String deleteEmployee(@RequestParam("employeeId") int id){
+        employeeRepository.deleteById(id);
         return "redirect:/employeeOverview";
     }
 
